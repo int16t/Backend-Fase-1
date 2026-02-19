2678ad7cb62baea7cbe919d81d61fbcf9f4f3f3c
int16t <gy4ra@protonmail.com>
2026-02-19 10:30:00 -0300

    feat: standardizing the code and adding new schema structure

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,25 +1,13 @@
 from typing_extensions import Annotated
 from app.database import SessionDep
-from app.models.model_tasks import Task
 from fastapi import APIRouter, HTTPException, Query
-from pydantic import BaseModel, Field
-from sqlmodel import select
 import app.crud.crud_tasks as crud
-
+import app.schemas.task_schemas as schemas
 
 router = APIRouter(
     prefix="/tasks",
     tags=["tasks"],
 )
-
-class Task_base(BaseModel):
-    id: int
-    title: str = Field(min_length=1, max_length=100)
-    description: str = Field(min_length=1, max_length=200)
-
-class Task_response(BaseModel):
-    title: str = Field(min_length=1, max_length=100)
-    description: str = Field(min_length=1, max_length=200)
 
 
 @router.get("/")
@@ -29,23 +17,27 @@ async def read_tasks(
     limit: Annotated[int, Query(le=100)] = 100,
     ):
     tasks = crud.get_tasks(session, offset, limit)
+    if not tasks:
+        raise HTTPException(status_code=404, detail="No tasks found")
     return tasks
 
 
 @router.get("/{task_id}")
 async def read_task(task_id: int, session: SessionDep):
     id_task = crud.get_task_by_id(session, task_id)
-    return id_task if id_task else HTTPException(status_code=404, detail="Task not found")
+    if not id_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return id_task
 
 
 @router.post("/create-task", status_code=201)
-async def create_task(task: Task_response, session: SessionDep) -> Task_response:
+async def create_task(task: schemas.Task_Create, session: SessionDep):
     db_task = crud.create_task(session, title=task.title, description=task.description)
     return db_task
 
 
 @router.put("/update-task/{task_id}", status_code=200)
-async def update_task(task_id: int, task: Task_response, session: SessionDep) -> Task_response:
+async def update_task(task_id: int, task: schemas.Task_Update, session: SessionDep):
     db_task = crud.update_task(session, task_id=task_id, title=task.title, description=task.description)
     if not db_task:
         raise HTTPException(status_code=404, detail="Task not found")
