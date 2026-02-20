6edeb64a5c0bf99e599d71f9fe154edb3f5df434
int16t <gy4ra@protonmail.com>
2026-02-19 23:48:35 -0300

    feat: creating new admin routes/controller

---
+++ app/routers/admin.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter
+from app.database import SessionDep
+import app.crud.crud_users as crud_users
+import app.crud.crud_tasks as crud_tasks
+import app.schemas.task_schemas as schemas
+
+router = APIRouter(
+    prefix="/admin",
+    tags=["admin"],
+    # dependencies=[Depends(require_admin_role)]
+)
+
+
+@router.get("/users")
+async def read_all_users(session: SessionDep):
+    return crud_users.get_users(session)
+
+
+@router.get("/tasks")
+async def read_all_tasks(session: SessionDep):
+    return crud_tasks.get_tasks(session)
+
+
+@router.post("/users/{user_id}/task")
+async def create_task_for_user(task: schemas.Task_Create, session: SessionDep):
+    return crud_tasks.create_task(session, title=task.title, description=task.description, user_id=task.user_id)
+
+
+@router.delete("/users/{user_id}", status_code=204)
+async def delete_user(user_id: int, session: SessionDep):
+    return crud_users.delete_user(session, user_id=user_id)
